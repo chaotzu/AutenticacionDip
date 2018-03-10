d4bca144b9a41c4c66f7da7dad20ccc81a389254
darkcore.7@gmail.com <44Tbo8B4fe>
2018-03-10 13:44:07 -0600

    dos

--- app/src/main/java/org/netzd/autenticaciondip/MainActivity.java
+++ app/src/main/java/org/netzd/autenticaciondip/MainActivity.java
@@ -2,6 +2,7 @@ package org.netzd.autenticaciondip;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +15,8 @@ import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Arrays;
@@ -33,7 +36,18 @@ public class MainActivity extends AppCompatActivity {
         FirebaseAuth aut = FirebaseAuth.getInstance(); //Instanciamos el objeto firebaseauth
 
         if(aut.getCurrentUser()!=null){
-
+            AuthUI.getInstance().signOut(this).addOnCompleteListener(
+                    new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Cerro Sesion", Toast.LENGTH_LONG).show();
+                            }else{
+                                Toast.makeText(MainActivity.this, "Hubo un problema", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+            );
         }else{
             startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(Arrays.asList(
                     new AuthUI.IdpConfig.EmailBuilder().build(),
